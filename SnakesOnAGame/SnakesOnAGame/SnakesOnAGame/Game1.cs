68564186c50362a6ebae99115a446ea4df8db69e
smoore8 <savionmoore8@gmail.com>
2014-04-14 10:10:57 -0400

    4/14/14

--- SnakesOnAGame/SnakesOnAGame/SnakesOnAGame/Game1.cs
+++ SnakesOnAGame/SnakesOnAGame/SnakesOnAGame/Game1.cs
@@ -18,29 +18,42 @@ namespace SnakesOnAGame
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
-        List <Vector2> snake = new List <Vector2>();
-            
+
+        Texture2D snakeTexture;
+        Texture2D FoodTexture;
+        Rectangle currentSquare;
+
+        float snakemovetimer = 0f;
+        float snakemovetime = 20f;
+
+        int playerScore = 0;
+
+        Random rand = new Random();
+        List<Vector2> snake = new List<Vector2>();
+        Vector2 Food = new Vector2(1, 2);
+        //Vector2 pellet = new Vector2(2, 2);//rand.Next(1, 10),rand.Next(1,10));
+        Vector2 velocity = new Vector2(0, -1);
+
+        Color[] colors = new Color[] { Color.Red, Color.PowderBlue,
+        Color.SteelBlue, Color.Tomato, Color.IndianRed, Color.DarkRed,Color.Wheat, Color.White,Color.Yellow,Color.Turquoise,Color.Green};
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
-
             Content.RootDirectory = "Content";
         }
 
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
+        /// related content. Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
 
-
-
             base.Initialize();
-
         }
 
         /// <summary>
@@ -51,6 +64,11 @@ namespace SnakesOnAGame
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            Food = new Vector2(45, 29);
+            snake.Add(new Vector2(40, 24));
+
+
 
             // TODO: use this.Content to load your game content here
         }
@@ -68,32 +86,93 @@ namespace SnakesOnAGame
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
         /// </summary>
-        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        /// <program name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
 
-           
             // TODO: Add your update logic here
 
-            KeyboardState kb = Keyboard.GetState();
-            base.Update(gameTime);
 
-            if (Kb.IsKeyDown(Keys.Up));
+            snakemovetimer += (float)gameTime.ElapsedGameTime.Milliseconds;
+
+            if (snakemovetimer > snakemovetime)
+            {
+
+                for (int n = snake.Count - 1; n > 0; n--)
+                {
+                    snake[n] = snake[n - 1];
+                }
+
+
+                snake[0] += velocity;
+                snakemovetimer = 0f;
+            }
+
+
+            KeyboardState kb = Keyboard.GetState();
+
+            if (kb.IsKeyDown(Keys.Up))
+            {
+                velocity = new Vector2(0, -1);
+            }
+
+            if (kb.IsKeyDown(Keys.Down))
+            {
+                velocity = new Vector2(0, 1);
+            }
+
+            if (kb.IsKeyDown(Keys.Left))
+            {
+                velocity = new Vector2(-1, 0);
+            }
+
+            if (kb.IsKeyDown(Keys.Right))
+            {
+                velocity = new Vector2(1, 0);
+
+            }
+
+
+
+
+
+
+            if (snake[0] == Food)
+            {
+                snake.Add(new Vector2(2, 1));
+                Food = new Vector2(rand.Next(1, 40), (rand.Next(1, 40)));
+                playerScore++;
+
+                //Food.Add(new Vector2(rand.Next(1,100), rand.Next(1,100)));
+            }
+
+
+            this.Window.Title = "Score : " + playerScore.ToString();
+
+            base.Update(gameTime);
         }
-        
+
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Draw(GameTime gameTime)
         {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
 
-            // TODO: Add your drawing code here
 
-            base.Draw(gameTime);
+            spriteBatch.Begin();
+            for (int i = 0; i < snake.Count; i++)
+            {
+                spriteBatch.Draw(FoodTexture, Food * 10, Color.White);
+
+                // TODO: Add your drawing code here
+
+                base.Draw(gameTime);
+            }
         }
     }
 }
